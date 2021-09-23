9109ce31c82c068dddac4b303b2593e3bfc89c7e
CT1020 <candicetsai2005@gmail.com>
2021-09-23 11:27:19 -0700

    Added in questions 1 to 4 and the final question at the end

--- tsai_candice_p1q1.py
+++ tsai_candice_p1q1.py
@@ -4,3 +4,104 @@
 # Quiz has 5 questions the user will answer.
 # It will keep track of score and give
 # A final result
+score = 0
+name = (input("Welcome to my quiz! What is your name? "))
+print(f'Alright {name}, lets get started shall we?')
+print()
+
+primogems = float(input('1. How many primogems does one fate cost? '))
+if primogems == 160:
+    print("Yes, well done!")
+    score = score + 1
+    print(f'Your score is {score}')
+else:
+    print("You'll get it next time.")
+    print("The answer is: 160")
+    print(f'Your score is {score}')
+
+print()
+print("-------------------------------------------------------")
+print('2. Which on of the following reactions is caused when applying cryo to electro? ')
+print("A. Melt")
+print("B. Crystalize")
+print("C. Burning.")
+print("D. Superconduct.")
+print("E. Vaporize.")
+reaction = input()
+if reaction == "Superconduct":
+    print("Nice job!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif reaction == "superconduct":
+    print("Nice job!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif reaction == "D":
+    print("Nice job!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif reaction == "d":
+    print("Nice job!")
+    score = score + 1
+    print(f'Your score is {score}')
+else:
+    print("That's wrong.")
+    print("The answer is: D. Superconduct")
+    print(f'Your score is {score}')
+
+print()
+print("-------------------------------------------------------")
+region = (input('3. What is the name of the 3rd and newest region in Teyvat? '))
+if region == "inazuma":
+    print("You got it right!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif region == "Inazuma":
+    print("You got it right!")
+    score = score + 1
+    print(f'Your score is {score}')
+else:
+    print("Darn")
+    print("The answer is: Inazuma")
+    print(f'Your score is {score}')
+
+print()
+print("-------------------------------------------------------")
+print('4. Tartaglia is which number of the Fatui Harbingers?')
+print("A. 13")
+print("B. 11")
+print("C. 17.")
+print("D. 7.")
+print("E. 9.")
+harbingers = input()
+if harbingers == "11":
+    print("Excellent!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif harbingers == "eleven":
+    print("Excellent!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif harbingers == "Eleven":
+    print("Excellent!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif harbingers == "b":
+    print("Excellent!")
+    score = score + 1
+    print(f'Your score is {score}')
+elif harbingers == "B":
+    print("Excellent!")
+    score = score + 1
+    print(f'Your score is {score}')
+else:
+    print("That was not the answer.")
+    print("The answer was D. 11")
+    print(f'Your score is {score}')
+
+print()
+print("-------------------------------------------------------")
+was_this_fun = (input('Was this a fun quiz? '))
+print(f'{was_this_fun}? Great to hear!')
+print()
+print("Your final score was:")
